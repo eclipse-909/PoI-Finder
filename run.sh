ac53a83c920902133ba0b8a9957d66be7b37108b
Ethan Morton <ethieq909@gmail.com>
2025-03-17 00:37:22 -0400

    updated run config

--- run.sh
+++ run.sh
@@ -1,4 +1,16 @@
 #! /bin/bash
 
+#compile typescript files
 tsc
+
+#if dist/public doesn't exist, create it
+if [ ! -d "dist/public" ]; then
+    mkdir -p dist/public
+fi
+
+#copy only html and css files from src/public to dist/public
+cp -r src/public/*.html dist/public
+cp -r src/public/*.css dist/public
+
+#run the server
 node dist/server/main.js
